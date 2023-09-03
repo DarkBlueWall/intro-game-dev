4b8f235e53acaf0ab4e9478d6c1004c574e1b4f0
DarkBlueWall <caseylbolton@gmail.com>
2023-09-04 04:16:05 +1000

    80%

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int i;
+    void Start()
+    {
+        i=3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + ":" + i.ToString());
+        i+=1;
+    }
+}
