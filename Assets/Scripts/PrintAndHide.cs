c3f1e5be1b7f960ca3dfa57aec863274ee30c792
DarkBlueWall <caseylbolton@gmail.com>
2023-09-04 04:40:23 +1000

    100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,15 +7,23 @@ public class PrintAndHide : MonoBehaviour
     // Start is called before the first frame update
     public Renderer rend;
     private int i;
+    private int target;
     void Start()
     {
+        target = Random.Range(150, 251);
         i=3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.name + ":" + i.ToString());
         i+=1;
+        Debug.Log(gameObject.name + ":" + i.ToString());
+        if (gameObject.tag == "red" && i==100) {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "blue" && i==100) {
+            gameObject.SetActive(false);
+        }
     }
 }
