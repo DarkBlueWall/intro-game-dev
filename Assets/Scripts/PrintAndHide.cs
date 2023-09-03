bc54d06d62f6f9bea760d64aa5e7d32ceb751666
DarkBlueWall <caseylbolton@gmail.com>
2023-09-04 04:29:14 +1000

    90%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int i;
+    public Renderer rend;
+    private int i;
     void Start()
     {
         i=3;
